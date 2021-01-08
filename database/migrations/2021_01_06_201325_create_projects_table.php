7a41a2a14538312d63901a047aa2ca631ef3e111
agatha.mathuva <agatha.mathuva@strathmore.edu>
2021-01-08 07:38:50 +0300

    first commit

---
+++ database/migrations/2021_01_06_201325_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('posts',255)->nullable();
+            $table->string('categories',100)->nullable();
+            $table->string('comments',500)->nullable();
+            $table->timestamps();
+            $table->integer('status')->default(0);
+
+
+           
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,37 @@
+extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>  {{ $project->name }}</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>posts:</strong>
+                {{ $project->posts }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>categories:</strong>
+                {{ $project->categories }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>comments:</strong>
+                {{ $project->comments }}
+            </div>
+        </div>
+        
+        
+@endsection
